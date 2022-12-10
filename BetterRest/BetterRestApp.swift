9ad51dd8535d30017c6eb1db919f2a1eefc0c017
thaddeus0605 <tdronski@gmail.com>
2022-12-09 22:32:12 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Thaddeus Dronski on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
