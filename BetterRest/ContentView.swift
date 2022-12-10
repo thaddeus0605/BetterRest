4f55b86e8af1d348c1109b2d4d8bf5b5195127bf
thaddeus0605 <tdronski@gmail.com>
2022-12-10 11:12:19 -0600

    uploaded coreML model to predict the best sleep time based on user inputs Added functionality to take user inputs and output a suggested bedtime base on the ML model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,14 +4,19 @@
 //
 //  Created by Thaddeus Dronski on 12/9/22.
 //
-
+import CoreML
 import SwiftUI
+
 
 struct ContentView: View {
     
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTite = ""
+    @State private var alertMSG = ""
+    @State private var showAlert = false
     
     var body: some View {
         //View
@@ -34,13 +39,39 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTite, isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMSG)
+            }
         }
     }
     
     //Functionality
     
     func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTite = "Your ideal bedtime is...."
+            alertMSG = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            //something went wrong
+            alertTite = "Error"
+            alertMSG = "Sorry there was an issue calculating bed time"
+        }
         
+        showAlert = true
     }
 }
 
